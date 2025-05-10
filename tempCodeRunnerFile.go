7c24364703e14206a6cf1286fcdcfe9253c32bb2
Исхак Мурадилдеев <iskhak@MacBook-Air-333.local>
2025-05-10 15:38:25 +0600

    Удалил временный файл tempCodeRunnerFile.go

--- tempCodeRunnerFile.go
+++
@@ -1,5 +0,0 @@
-const (
-	mongoURI      = "mongodb+srv://nagidevfullstack:Hadi2017g@golangdb.ebvlakf.mongodb.net/?retryWrites=true&w=majority&appName=GolangDB"
-	databaseName  = "TodoListONGolang"
-	collectionName = "GolangDB"
-)
